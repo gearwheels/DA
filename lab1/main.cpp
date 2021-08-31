c3373f01cd4117cca900dee0c97a5b6a26cbd3b6
gearwheels <70712596+gearwheels@users.noreply.github.com>
2021-08-31 19:15:32 +0300

    Add files via upload

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -14,8 +14,8 @@ struct Titem {
 template<class T>
 class TVector {
 	private:
-		unsigned int capacity;
-		unsigned int maxsize;
+		unsigned int size; // кол-во занятых ячеек 
+		unsigned int capacity; // емкость 
 		T* data;
 	public:
 		void Assert(const unsigned int n, T elem);
@@ -43,48 +43,40 @@ T& TVector<T>::operator[](const unsigned int &index) {
 template<class T>
 void TVector<T>::PushBack(T elem) {
 	if (data == 0) {
-		maxsize = 1;
-		data = new T[maxsize];
+		capacity = 1;
+		data = new T[capacity];
 	}
-	if (capacity == maxsize) {
-		maxsize = maxsize * 2;
-		T* new_data = new T[maxsize];
-		for (int i = 0; i < capacity; i++) {
+	if (size == capacity) {
+		capacity = capacity * 2;
+		T* new_data = new T[capacity];
+		for (int i = 0; i < size; i++) {
 			new_data[i] = data[i];
 		}
 		delete[] data;
 		data = new_data;
 	}
-	data[capacity] = elem;
-	capacity++;
+	data[size] = elem;
+	size++;
 }
 
 template<class T>
 unsigned int TVector<T>::Size() {
-	return capacity;
+	return size;
 }
 
 template<class T>
 TVector<T>::TVector() {
+	size = 0;
 	capacity = 0;
-	maxsize = 0;
 	data = 0;
 }
 
 template<class T>
 TVector<T>::TVector(const unsigned int n) {
+	size = n;
 	capacity = n;
-	maxsize = n;
-	data = new T[capacity];
+	data = new T[size];
 	Assert(n, T());
-}
-
-template<class T>
-TVector<T>::TVector(const unsigned int n, T elem) {
-	capacity = n;
-	maxsize = n;
-	data = new T[capacity];
-	Assert(n, elem);
 }
 
 template<class T>
@@ -92,44 +84,39 @@ TVector<T>::~TVector() {
 	delete[] data;
 }
 
-
-void RadixSort(TVector<Titem> &data, int n) {
+void RadixSort(TVector<Titem> &data, int n) { // макс число 18 446 744 073 709 551 615(8 байт)
 	unsigned long long div = 1;
 	for (int k = 0; k < 4; k++) {
-		TVector<int> count(MOD);
-		for (int i = 0; i < n; i++) {
-			count[(data[i].key / div) % MOD]++;
-		}
+		TVector<int> count(MOD); // создаем массив счетчиков в диапазоне от 1 до 65536(2 байта)
+		for (int i = 0; i < n; i++) { // проходим по массиву
+			count[(data[i].key / div) % MOD]++; // по остатку от деления на 65536 берем в счетчике ячейку и делпем ++
+		}// (сначала отсекаем уже пройденую часть числа делением на div)
 
-		for (int i = 1; i < MOD; i++) {
+		for (int i = 1; i < MOD; i++) {// в count собираем сложением индекс массива data последнего числа, соответствующего индексу count
 			count[i] = count[i] + count[i - 1];
 		}
 		TVector<Titem> res(n);
-		for (int i = n - 1; i >= 0; i--) {
-			res[count[(data[i].key / div) % MOD] - 1] =  data[i];
-			count[(data[i].key / div) % MOD]--;
+		for (int i = n - 1; i >= 0; i--) { // идем с конца массива
+			res[count[(data[i].key / div) % MOD] - 1] =  data[i];// копируем в res из data на позицию полученную в count
+			count[(data[i].key / div) % MOD]--; // уменьшаем позицию полученную в count
 		}
 		for (int i = 0; i < n; i++) {
-			data[i] = res[i];
+			data[i] = res[i]; // копируем в data
 		}
 		div = div * MOD;
 	}
 }
 
 int main() {
-	double start, end;
-	start = clock();
 	TVector<Titem> a;
-	Titem cur;
-	while (scanf("%llu%s", &cur.key, cur.value) > 0) {
-        a.PushBack(cur);
+	Titem data;
+	while (scanf("%llu%s", &data.key, data.value) > 0) {
+        a.PushBack(data);
     }
 	int n = a.Size();
 	RadixSort(a, n);
 	for (int i = 0; i < n; i++) {
 		printf("%llu %s\n", a[i].key, a[i].value);
 	}
-	end = clock();
-    printf("TIME %Lf\n", (end - start) /1000);
 	return 0;
 }
